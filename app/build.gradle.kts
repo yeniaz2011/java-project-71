a53b6440e76ce0b129a52c7697b8a1e6136371a7
xenia <2b4got10@mail.ru>
2025-12-12 17:28:53 +0500

    2 step

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+    id("application")
+
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
